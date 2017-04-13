8942c85f93ef103f28e95dacd66beafd90316058
Roberto Torres <roberto.torres.dev@gmail.com>
2017-04-13 11:42:12 -0700

    updating setup file

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
   name = 'webelement_highlighter',
   packages = ['webelement_highlighter'],
-  version = '0.1',
+  version = '0.5',
   description = 'Library to highlight WebElements',
   author = 'Roberto Torres',
   author_email = 'roberto.torres.dev@gmail.com',
   url = 'https://github.com/rtorres90/webelement_highlighter',
-  download_url = 'https://github.com/rtorres90/webelement_highlighter/archive/0.1.tar.gz',
+  download_url = 'https://github.com/rtorres90/webelement_highlighter/archive/0.5.tar.gz',
   keywords = ['webdriver', 'selenium', 'testing', 'automation'],
   classifiers = ['Programming Language :: Python :: 2.7'],
 )
